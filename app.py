8a9427a7f2c9ad5b87bdd64074e5cc277ebe9c56
RIyer <rohan.s.iyer@uscga.edu>
2024-10-26 09:20:37 -0400

    commit

--- app.py
+++ app.py
@@ -1,40 +1,29 @@
 # Import required libraries
-from dash import Dash, dcc, html
+import streamlit as st
 import plotly.graph_objs as go
-
-# Initialize the app
-app = Dash(__name__)
 
 # Sample data for the line graph
 x_values = ['Jan', 'Feb', 'Mar', 'Apr', 'May']
 y_values = [10, 15, 8, 25, 18]
 
-# Define the layout of the app
-app.layout = html.Div([
-    html.H1("Basic Line Graph"),
-    dcc.Graph(
-        id="line-graph",
-        figure={
-            'data': [
-                go.Scatter(
-                    x=x_values,
-                    y=y_values,
-                    mode='lines+markers',
-                    name='Sample Data'
-                )
-            ],
-            'layout': go.Layout(
-                title="Monthly Data",
-                xaxis={'title': 'Month'},
-                yaxis={'title': 'Value'},
-                template="plotly_dark"
-            )
-        }
+# Create a Plotly figure
+fig = go.Figure(
+    data=go.Scatter(
+        x=x_values,
+        y=y_values,
+        mode='lines+markers',
+        name='Sample Data'
     )
-])
+)
 
-# Run the app
-server = app.server
+# Customize layout
+fig.update_layout(
+    title="Monthly Data",
+    xaxis_title="Month",
+    yaxis_title="Value",
+    template="plotly_dark"
+)
 
-if __name__ == '__main__':
-    app.run_server()
+# Streamlit app layout
+st.title("Basic Line Graph")
+st.plotly_chart(fig)
